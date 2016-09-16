56a3df562bed12281b7e4d455657114670e54df9
Martin Marko <marcus@gratex.com>
2016-09-16 16:54:11 +0200

    - another shorter version

--- src/extract.js
+++ src/extract.js
@@ -6,40 +6,23 @@ er;arrowWidth=0.275;arrowSize=0.07542372881355933;strokeColor=#000000;rotation=9
 singleArrow:TOPO
 
 */
-var str = [];
 var byline = require("byline");
-byline(process.stdin /*,{keepEmptyLines:true}*/ ) //
-    .on("data", function(line) {
-        str.push(line += "");
-        //console.error("data:", line);
-    }).on("end", function() {
-        parseStarletOut(str.join("$"));
+byline(process.stdin, { keepEmptyLines: false })
+    .on("data", function(line) {;
+        parseLine(line + "");
     });
 
+
+// NTH module maybe
 var style2json = require("../lib/style2json.js");
 
+function parseLine(line) {
 
+    var parts = line.split('|');
+    var label = parts[0] || "";
+    var style = parts[1] || "";
 
-function parseStarletOut(str) {
-    //console.error("str", str);
-    var arrSplit = str.split('$'); // split value-style string literals
-    var arrVS = [];
-    var obj = {};
-    var component = {};
-
-
-    arrSplit.forEach(function(value) {
-        if (value && value.trim().length) {
-            arrVS = value.split('|'); // split value from style
-            // check style string
-
-            if (arrVS[1] && arrVS[1].indexOf("shape") !== 1) {
-                obj = style2json(arrVS[1]);
-                if (arrVS[0] && arrVS[0].trim().length) {
-                    label = arrVS[0];
-                    console.log(obj.shape + ":" + label);
-                }
-            }
-        }
-    });
+    // TODO: test for non shapes, blind rewrite
+    var shapeObj = ~style.indexOf("shape") && style2json(style);
+    shapeObj && console.log(`${label}:${shapeObj.shape}`);
 }
